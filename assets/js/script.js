9b8ddfeac3fd069778ee59fe9561c7c9426d29ca
Dennis Leyva <dennisleyva8@gmail.com>
2021-11-25 15:47:46 -0700

    new layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,21 @@
-var counter = 10
-var countdown = function(){
-    console.log(counter);
-    counter--;
-    if (counter === 0){
-        console.log("blastoff");
-        clearInterval(startCountdown);
-    };
-};
 
-var startCountDown = setInterval(countdown, 1000);
+
+//this is my timer to start the quiz it doesnt work
+function startTimer(){
+var counter = 5;
+setInterval(function() {
+    counter--;
+    if (counter >= 0) {
+        span = document.getElementById("count");
+        span.innerHTML = "counter";
+    }
+    if (counter === 0) {
+        alert('sorry, out of time');
+        clearInterval(counter);
+    }
+}, 1000);
+}
+function start(){
+    document.getElementById("count").style="color:green;";
+    startTimer();
+};
